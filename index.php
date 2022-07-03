9a4ecda0e5f47452ef7cfd14fa62bfff956914c0
Nikita <example@gmail.com>
2022-07-03 21:11:59 +0300

    architetra first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once('init.php');
+
+use System\Exceptions\Exc404;
+use System\Router;
+use System\ModulesDispatcher;
+
+use Modules\Articles\Module as Articles;
+
+const BASE_URL = '/Framework/';
+
+try{	
+	$modules = new ModulesDispatcher();
+	$modules->add(new Articles());
+	$router = new Router(BASE_URL);
+
+	$modules->registerRoutes($router);
+/* 	 */
+
+	$uri = $_SERVER['REQUEST_URI'];
+	$activeRoute = $router->resolvePath($uri);
+
+	$c = $activeRoute['controller'];
+	$m = $activeRoute['method'];
+
+	$c->$m();
+	$html = $c->render();
+	echo $html;
+}
+catch(Exc404 $e){
+	echo '404'; 
+}
+catch(Throwable $e){
+	echo 'nice show error - ' . $e->getMessage();
+}
